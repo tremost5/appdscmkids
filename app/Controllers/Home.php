19d7df77c25e5fb914cceb8b2228e7607fbedd3d
tremost5 <tremostayang5@gmail.com>
2026-03-06 00:01:48 +0700

    Redirect local home route to login page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends BaseController
 {
     public function index()
     {
+        $host = (string) $this->request->getServer('HTTP_HOST');
+        $isLocalHost = str_starts_with($host, '127.0.0.1') || str_starts_with($host, 'localhost');
+
+        if (app()->environment('local') || $isLocalHost) {
+            return redirect()->to('/login');
+        }
+
         return view('home/index');
     }
 }
