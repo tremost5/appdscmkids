b496d8637032e31f2d62c89161b579f30697cd25
tremost5 <tremostayang5@gmail.com>
2026-03-06 00:40:12 +0700

    Redirect root to login and remove legacy moved-address landing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,13 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        $host = (string) ($_SERVER['HTTP_HOST'] ?? '');
-        $isLocalHost = str_starts_with($host, '127.0.0.1') || str_starts_with($host, 'localhost');
-
-        if (app()->environment('local') || $isLocalHost) {
-            return redirect()->to('/login');
-        }
-
-        return view('home/index');
+        return redirect()->to('/login');
     }
 }
